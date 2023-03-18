0caee90533bac019f27ebcc11c65bdbc288eeebf
Максим Каверин <k1sper777@mail.ru>
2023-03-18 12:17:10 +0300

    jkjkj

--- Practic 1/Page1.xaml.cs
+++ Practic 1/Page1.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Practic_1.DataSet1TableAdapters;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +20,30 @@ namespace Practic_1
 
     public partial class Page1 : Page
     {
+        
         CarsTableAdapter Cars = new CarsTableAdapter();
+        
         public Page1()
         {
             InitializeComponent();
+            CarsGrid.ItemsSource = Cars.GetData();
+            CarsComboBox.ItemsSource = Cars.GetData();
+            CarsComboBox.DisplayMemberPath = "Name";
+            CarsComboBox.SelectedValuePath = "Id";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            short Price = Convert.ToInt16(CarsPrice.Text);
+            Cars.InsertQuery(CarsName.Text,Price,CarsColor.Text);
+            CarsGrid.ItemsSource = Cars.GetData();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int id = (int)(CarsGrid.SelectedItem as DataRowView).Row[0];
+            Cars.DeleteQuery(id);
             CarsGrid.ItemsSource = Cars.GetData();
         }
     }
--- Practic 1/Page2.xaml.cs
+++ Practic 1/Page2.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Practic_1.DataSet1TableAdapters;
+﻿using MaterialDesignThemes.Wpf;
+using Practic_1.DataSet1TableAdapters;
+using System.Data;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -12,6 +14,23 @@ namespace Practic_1
         public Page2()
         {
             InitializeComponent();
+            BuyersGrid.ItemsSource = Buyers.GetData();
+            BuyersComboBox.ItemsSource = Buyers.GetData();
+            BuyersComboBox.DisplayMemberPath = "Name";
+            BuyersComboBox.SelectedValuePath = "Id";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int id = (int)BuyersComboBox.SelectedValue;
+            Buyers.InsertQuery(NameUser.Text, SurnameUser.Text, id);
+            BuyersGrid.ItemsSource = Buyers.GetData();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int id = (int)(BuyersGrid.SelectedItem as DataRowView).Row[0];
+            Buyers.DeleteQuery(id);
             BuyersGrid.ItemsSource = Buyers.GetData();
         }
     }
